1df314bd5ac0dea21d3e784844a16b25667a7872
Jeanette Conklin <conklinj09@gmail.com>
2022-10-13 21:25:51 -0700

    Version 3.0

---
+++ seeds/prophecyDb.js
@@ -0,0 +1,20 @@
+//emotions data
+//tags for 
+//angry 
+//sad afraid 
+//happy excited
+
+module.exports = [
+    {
+        "prophecy_str": "this is where prophecy_str will go",
+        "emotions": "sad"
+    },
+    {
+        "prophecy_str": "mad test",
+        "emotions": "mad"
+    },
+    {
+        "prophecy_str": "happy test",
+        "emotions": "happy"
+    },
+]
